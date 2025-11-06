e77445e8a6ff895b3fb9d9d60d1254bb1c1973d2
Jossnnier <fauconnierjosselin@yahoo.fr>
2025-11-06 14:10:39 +0100

    nav + image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
 import Header from './components/Header/header';
+import NavBar from './components/Nav-bar/nav-bar';
 import './App.css';
+import { use, useState } from 'react';
 
 function App() {
-    return (
-        <div className="App">
+  const [pairCount, setPairCount] = useState(6);
+
+  const handlePairCountChange = (count) => {
+    setPairCount(count);
+  }
+    
+  return (
+        <div >
             <Header/> 
             <main>
+              <NavBar onPairCountChange={handlePairCountChange}/>
+              
                
             </main>
         </div>
--- src/components/Nav-bar/nav-bar.jsx
+++ src/components/Nav-bar/nav-bar.jsx
@@ -1,1 +1,22 @@
 import "./nav.css";
+
+const NavBar = ({ onPairCountChange }) => {
+    const pairOptions = [4, 6, 8, 10, 12];
+
+    return (
+        <nav className="navbar">
+            <div className="pair-options">
+                {pairOptions.map((option) => (
+                    <button
+                        key={option}
+                        onClick={() => onPairCountChange(option)}
+                    >
+                        {option} Paires
+                    </button>
+                ))}
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
